8c2e61afd812b521043e334ca7d6dd8d98dd82fd
Kyle Mathewson <kylemath@gmail.com>
2020-03-14 15:28:37 -0700

    changes to make it print by province, then a graph

--- pycovid/pycovid.py
+++ pycovid/pycovid.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-def getCovidCases(countries=None, start_date=None, end_date=None, casetype=['confirmed', 'death', 'recovered']):
+def getCovidCases(countries=None, provinces=None, start_date=None, end_date=None, casetype=['confirmed', 'death', 'recovered'], cumsum=False):
     df = pd.read_csv('https://raw.githubusercontent.com/RamiKrispin/coronavirus-csv/master/coronavirus_dataset.csv',
             names=["province_state", 'country_region', 'lat', 'long', 'date', 'cases', 'type'], skiprows=1, parse_dates=['date'])
     
@@ -12,12 +12,23 @@ def getCovidCases(countries=None, start_date=None, end_date=None, casetype=['con
         df = df[df.date >= start_date]
     if end_date is not None:
         df = df[df.date <= end_date]
+
+    if provinces is not None:
+        for province in provinces:
+            if province not in df.province_state.values:
+                print("Province: {0} not found in database. Check spelling!".format(province))
+            df = df[(df.province_state.isin(provinces))] 
             
     if countries is not None:
         for country in countries:
             if country not in df.country_region.values:
                 print("Country: {0} not found in database. Check spelling!".format(country))
             df =  df[(df.country_region.isin(countries))]
+
+    if cumsum is True:
+        df.cases = df.groupby('province_state')['cases'].transform(pd.Series.cumsum)
+            
+
             
     iso_df = pd.read_csv('https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/slim-3/slim-3.csv')
     iso_df = iso_df[['name', 'alpha-3']]
@@ -52,7 +63,7 @@ def getCovidCasesWide(countries=None, start_date=None, end_date=None, casetype=[
     else:
         if end_date is not None:
             df = df[df.date <= end_date]
-            
+    
     if countries is not None:
         for country in countries:
             if country not in df.country_region.values:
@@ -112,16 +123,23 @@ def getIntervalData(df, interval='30D'):
 
 def plot_countries(df=None, grouped_data=False, metric="confirmed"):
     
-    if df is None:
+    if df == None:
         df = getCovidCasesWide()
    
     if grouped_data == False:
         df = df.groupby(['country_region', 'alpha-3']).sum().reset_index()
         
     fig = px.choropleth(df, locations="alpha-3",
-                    color=metric, # lifeExp is a column of gapminder
+                    color="confirmed", # lifeExp is a column of gapminder
                     hover_name="country_region", # column to add to hover information
-                    color_continuous_scale='RdBu_r')
+                    color_continuous_scale="OrRd")
     fig.show()
     
+
+def plot_provinces(provinces=None, start_date=None, end_date=None, casetype=['confirmed', 'death', 'recovered']):
+    
+    df = getCovidCases(countries = ['Canada'], provinces = provinces, casetype = casetype, start_date=start_date, end_date=end_date, cumsum=True)
+    fig = px.line(df, x="date", y="cases", color='province_state', title="Number of confirmed COVID-19 cases over time")
+    fig.show()
+
     
