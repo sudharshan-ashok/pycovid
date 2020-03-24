2cc99dbbac4e2ab3f4d1fe71594bccf568c27dc1
Kyle Mathewson <kylemath@gmail.com>
2020-03-23 17:01:03 -0700

    updated

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 from pycovid import pycovid
 
 
-# pycovid.plot_countries_trend(countries=['Korea, South'],
-# 			casetype=['confirmed'], start_date="2020-01-01", plottype="linear")
+pycovid.plot_countries_trend(countries=['Spain', 'Germany', 'Portugal', 'Bulgaria', 'France', 'Netherlands'],
+			casetype=['confirmed'], start_date="2020-01-01", plottype="linear")
 
 pycovid.plot_provinces(country=['China'],  
 			casetype=['confirmed'], start_date="2019-10-01", plottype="linear",
-			proportion=False, cumulative=False)
+			proportion=True, cumulative=True)
 pycovid.plot_provinces(country=['Australia'],  
 			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
-			proportion=False, cumulative=False)
+			proportion=True, cumulative=True)
 pycovid.plot_provinces(country=['US'],  
 			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
-			proportion=False, cumulative=False)
+			proportion=True, cumulative=True)
 pycovid.plot_provinces(country=['Canada'],  
 			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
-			proportion=False, cumulative=False)
+			proportion=True, cumulative=True)
 
 # pycovid.plot_countries()
 
