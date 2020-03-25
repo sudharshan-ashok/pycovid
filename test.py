2e5c4f514ec10e05774276802763adbc442eaae6
Kyle Mathewson <kylemath@gmail.com>
2020-03-24 19:58:01 -0700

    added diamond princess to canada

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
 from pycovid import pycovid
 
 
-pycovid.plot_countries_trend(countries=['Spain', 'Germany', 'Portugal', 'Bulgaria', 'France', 'Netherlands'],
-			casetype=['confirmed'], start_date="2020-01-01", plottype="linear")
+# pycovid.plot_countries_trend(countries=['Spain', 'Germany', 'Portugal', 'Bulgaria', 'France', 'Netherlands'],
+# 			casetype=['confirmed'], start_date="2020-01-01", plottype="linear")
 
-pycovid.plot_provinces(country=['China'],  
-			casetype=['confirmed'], start_date="2019-10-01", plottype="linear",
-			proportion=True, cumulative=True)
-pycovid.plot_provinces(country=['Australia'],  
-			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
-			proportion=True, cumulative=True)
-pycovid.plot_provinces(country=['US'],  
-			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
-			proportion=True, cumulative=True)
+# pycovid.plot_provinces(country=['China'],  
+# 			casetype=['confirmed'], start_date="2019-10-01", plottype="linear",
+# 			proportion=True, cumulative=True)
+# pycovid.plot_provinces(country=['Australia'],  
+# 			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
+# 			proportion=True, cumulative=True)
+# pycovid.plot_provinces(country=['US'],  
+# 			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
+# 			proportion=True, cumulative=True)
 pycovid.plot_provinces(country=['Canada'],  
 			casetype=['confirmed'], start_date="2019-12-01", plottype="linear",
 			proportion=True, cumulative=True)
 
-# pycovid.plot_countries()
+pycovid.plot_countries()
 
 
