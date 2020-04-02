eb33e4b9aa707364072dd375afe6c6ee47555d0c
Kyle Mathewson <kylemath@gmail.com>
2020-04-01 21:29:22 -0700

    better plot

--- pycovid/pycovid.py
+++ pycovid/pycovid.py
@@ -173,20 +173,75 @@ def plot_provinces(country=None, provinces=None, start_date=None, end_date=None,
 
     fig = px.line(df, x="daysFrom100", y="cases", color='province_state', title=title)
 
+    fig.add_shape(
+        type="line",
+        x0=0,
+        y0=100,
+        x1=7,
+        y1=12800,
+        line=dict(
+            color="Gray",
+            width=2,
+            dash="dot",
+        )
+    )
+
+    fig.add_shape(
+        type="line",
+        x0=0,
+        y0=100,
+        x1=14,
+        y1=12800,
+        line=dict(
+            color="Gray",
+            width=2,
+            dash="dot",
+        )
+    )
+
+    fig.add_shape(
+        type="line",
+        x0=0,
+        y0=100,
+        x1=18,
+        y1=6400,
+        line=dict(
+            color="Gray",
+            width=2,
+            dash="dot",
+        )
+    )
+
+    fig.add_shape(
+        type="line",
+        x0=0,
+        y0=100,
+        x1=28,
+        y1=800,
+        line=dict(
+            color="Gray",
+            width=2,
+            dash="dot",
+        )
+    )
+
+
     fig.update_layout(
         yaxis_title=ylabel,
         yaxis = dict(
             showexponent = 'all',
-            exponentformat = 'e',
-            type = plottype
+            exponentformat = 'none',
+            type = plottype,
+            range = [2, 4]
         ),
         xaxis = {
             'tickmode': 'auto',
-            'nticks': 30
+            'nticks': 30,
+            'range': [0, 18]
         }
-    
     )
 
+    fig.update_shapes(dict(xref='x', yref='y'))
     fig.show()
 
     
